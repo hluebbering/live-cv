942890cff5e316652e7f604f2b9aa1985b4e8bbf
Hannah Luebbering <luebhr@gmail.com>
2020-12-28 10:29:49 -0700

    r script

---
+++ resume.R
@@ -0,0 +1,190 @@
+library(latex2exp)
+knitr::opts_chunk$set(
+  results='asis', 
+  echo = FALSE
+)
+
+
+CRANpkg <- function (pkg) {
+  cran <- "https://CRAN.R-project.org/package"
+  fmt <- "[%s](%s=%s)"
+  sprintf(fmt, pkg, cran, pkg)
+}
+
+Biocpkg <- function (pkg) {
+  sprintf("[%s](http://bioconductor.org/packages/%s)", pkg, pkg)
+}
+
+library(glue)
+library(tidyverse)
+
+# Set this to true to have links turned into footnotes at the end of the document
+PDF_EXPORT <- FALSE
+
+# Holds all the links that were inserted for placement at the end
+links <- c()
+
+find_link <- regex("
+  \\[   # Grab opening square bracket
+  .+?   # Find smallest internal text as possible
+  \\]   # Closing square bracket
+  \\(   # Opening parenthesis
+  .+?   # Link text, again as small as possible
+  \\)   # Closing parenthesis
+  ",
+                   comments = TRUE)
+
+sanitize_links <- function(text){
+  if(PDF_EXPORT){
+    str_extract_all(text, find_link) %>% 
+      pluck(1) %>% 
+      walk(function(link_from_text){
+        title <- link_from_text %>% str_extract('\\[.+\\]') %>% str_remove_all('\\[|\\]') 
+        link <- link_from_text %>% str_extract('\\(.+\\)') %>% str_remove_all('\\(|\\)')
+        
+        # add link to links array
+        links <<- c(links, link)
+        
+        # Build replacement text
+        new_text <- glue('{title}<sup>{length(links)}</sup>')
+        
+        # Replace text
+        text <<- text %>% str_replace(fixed(link_from_text), new_text)
+      })
+  }
+  
+  text
+}
+
+
+# Takes a single row of dataframe corresponding to a position
+# turns it into markdown, and prints the result to console.
+build_position_from_df <- function(pos_df){
+  
+  missing_start <- pos_df$start == 'N/A'
+  content_dash <- "$\\bf\\hbox{-}$"
+  dates_same <- pos_df$end == pos_df$start
+  if (pos_df$end == 9999) {
+    pos_df$end = "present"
+  }
+  if(any(c(missing_start,dates_same))){
+    timeline <- pos_df$end
+  } else {
+    timeline <- glue('{pos_df$start}', '{content_dash}', ' {pos_df$end}')
+  }
+  
+  descriptions <- pos_df[str_detect(names(pos_df), 'description')] %>% 
+    as.list() %>% 
+    map_chr(sanitize_links)
+  
+  # Make sure we only keep filled in descriptions
+  description_bullets <- paste('-', descriptions[descriptions != 'N/A'], collapse = '\n')
+  
+  if (length(description_bullets) == 1 && description_bullets == "- ") {
+    description_bullets <- ""
+  }
+  glue(
+    "### {sanitize_links(pos_df$title)}
+
+{pos_df$loc}
+
+{pos_df$institution}
+
+{timeline}
+
+{description_bullets}
+
+
+"
+  ) %>% print()
+}
+
+# Takes nested position data and a given section id 
+# and prints all the positions in that section to console
+print_section <- function(position_data, section_id){
+  x <- position_data %>% 
+    filter(section == section_id) %>% 
+    pull(data) 
+  
+  prese <- " - "
+  xx <- list()
+  
+  for (i in seq_along(x)) {    
+    y = x[[i]]
+    y <- cbind(y, start2 = as.character(y$start))
+    y <- cbind(y, end2 = as.character(y$end))
+    
+    se <- paste(y$start, "-", y$end, collapse = " ")
+    if (prese == se) {
+      y$start2 = ""
+      y$end2 = ""
+    } else {
+      prese = se
+    }
+    
+    xx[[i]] <- select(y, -c(start, end)) %>%
+      rename(start=start2, end=end2)
+  }
+  
+  xx %>% 
+    purrr::walk(build_position_from_df)
+}
+
+
+
+fill_nas <- function(column){
+  ifelse(is.na(column), 'N/A', column)
+}
+
+# Load csv with position info
+position_data <- read_csv('positions.csv') %>% 
+  mutate_all(fill_nas) %>% 
+  arrange(order, desc(end)) %>% 
+  mutate(id = 1:n()) %>% 
+  nest(data = c(-id, -section))
+# When in export mode the little dots are unaligned, so fix that. 
+if(PDF_EXPORT){
+  cat("
+  <style>
+  :root{
+    --decorator-outer-offset-left: -6.5px;
+  }
+  </style>")
+}
+# When in export mode the little dots are unaligned, so fix that. 
+if(PDF_EXPORT){
+  cat("View this CV online with links at _guangchuangyu.github.io/cv_")
+}
+print_section(position_data, 'education')
+print_section(position_data, 'research_positions')
+print_section(position_data, 'volunteer_positions')
+ggtree <- "ggtree (Yu et al. 2017)"
+url <- 'http://onlinelibrary.wiley.com/doi/10.1111/2041-210X.12628/abstract'
+
+ggtree2 <- glue("[{ggtree}]({url})")
+journal <- "**_Methods in Ecology and Evolution_**"
+
+url2 <- "https://methodsblog.com/2020/11/19/ggtree-tree-visualization/"
+journal2 <- glue("[{journal}]({url2})")
+
+glue(
+  " 
+  The {ggtree2} paper was selected as a feature article to celebrate the 10^th^ anniversary of the launch of {journal2}.
+  "
+) %>% print()
+print_section(position_data, 'academic_projects')
+print_section(position_data, 'affliation')
+if(PDF_EXPORT){
+  cat("
+  
+Links {data-icon=link}
+--------------------------------------------------------------------------------
+
+
+
+")
+  
+  walk2(links, 1:length(links), function(link, index){
+    print(glue('{index}. {link}'))
+  })
+}
